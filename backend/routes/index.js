912b8fa31683968d00cd860d7cf1693a59211716
Aman Aggarwal <akaggarw@ucsd.edu>
2022-02-10 15:20:56 -0800

    Update sample add user payload and endpoint name

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,13 +5,14 @@ routes.get('/hello', (req, res) => { // Only for testing
   res.json({message: 'Hello world! Lakta here'})
 })
 
-routes.post('/add-user', (req, res) => { // Only for testing
+routes.post('/user', (req, res) => { // Only for testing
   // TODO Remove this endpoint when starting API development
   const sampleUser = User({
-    firstName: 'Sample',
+    firstName: 'Test',
     lastName: 'User',
     password: 'password',
     phone: 'number',
+    location: 'location'
   });  
   sampleUser.save((err) => {
     console.error('Error occured while saving', err);
