2955c45b0ae0ea011132186a22bd0f1b5dfb20a2
Aman Aggarwal <akaggarw@ucsd.edu>
2022-02-26 14:29:16 -0800

    WIP Add update job service

---
+++ backend/routes/errors.js
@@ -0,0 +1,5 @@
+const INVALID_OBJECT_ID_RECEIVED_MSG = 'Error: Invalid ID was found, ID must be a string of 12 bytes or a string of 24 hex characters';
+
+module.exports = {
+    INVALID_OBJECT_ID_RECEIVED_MSG
+}
--- backend/routes/job.js
+++ backend/routes/job.js
@@ -5,10 +5,12 @@ const uploadImage = require('../middlewares/upload');
 
 const {SAMPLE_USER} = require('../constants');
 const JobService = require('../services/job');
+const errors = require('./errors');
+const { Types } = require('mongoose');
 
 const upload = multer({ dest: 'images/' }) // Uploads files to images/<path>
 
-routes.post('/job', uploadImage, (req, res) => { // Create a new job
+routes.post('/jobs', uploadImage, (req, res) => { // Create a new job
     console.info('Posting new Job')
     // console.log('req: ', req.body); // Request text parameters  
     // console.log('req:', req.files) // Images in request
@@ -17,27 +19,37 @@ routes.post('/job', uploadImage, (req, res) => { // Create a new job
         SAMPLE_USER,
         req.body,
         req.files,
-    ).then((job) => {res.status(200).json({ job })})
+    ).then((job) => {res.status(200).send(job)})
 });
 
-routes.put('/job/:jobid', (req, res) => { // Update an existing job
-    console.info('Updating existing job')
-    let id = 0;
+routes.put('/jobs/:jobid', uploadImage, (req, res) => {
+    if (!Types.ObjectId.isValid(req.params.jobid)) {
+        res.status(400).json({message: errors.INVALID_OBJECT_ID_RECEIVED_MSG})
+    }
+    const jobId = Types.ObjectId(req.params.jobid);
+    console.info('Updating job with ID: ', jobId);
+
     JobService.updateJob(
-        id,
+        SAMPLE_USER,
+        jobId,
         req.body,
         req.files,
-    ).then((job) => {res.status(200).json({ job })})
+    ).then((originalJob) => {res.status(501).send()})
 });
 
-routes.delete('/job/:jobid', (req, res) => { // Delete an existing job
+routes.get('/jobs/:jobid', (req, res) => {
+    if (!Types.ObjectId.isValid(req.params.jobid)) {
+        res.status(400).json({message: errors.INVALID_OBJECT_ID_RECEIVED_MSG})
+    }
+    const jobId = Types.ObjectId(req.params.jobid);
+    console.info('Getting job with ID: ', jobId);
+    JobService.getJob(jobId).then((job) => res.status(200).send(job));
+})
+
+routes.delete('/jobs/:jobid', (req, res) => { // Delete an existing job
     console.info('Deleting a job');
     let id = 0;
-    JobService.deleteJob(id).then((job) => {res.status(200).json({ job })})
-});
-
-routes.get('/job/:jobid', (req, res) => { // Get an existing job
-    console.info('Getting a job');
+    // JobService.deleteJob(id).then((job) => {res.status(200).json({ job })})
 });
 
 module.exports = routes;
