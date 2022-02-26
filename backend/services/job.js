664c52304c775678fe5772518d9bada152f7ec2c
Aman Aggarwal <akaggarw@ucsd.edu>
2022-02-26 14:29:16 -0800

    WIP Add update job service

--- backend/services/job.js
+++ backend/services/job.js
@@ -4,48 +4,40 @@ const JobModel = require('../models/job');
 async function createJob(user, jobData, jobImages) {
     console.log('Images:', jobImages);
     console.log('Data: ', jobData);
-
+    
     let job = JobModel({
         ...jobData, //TODO Sanitize input
         client: user._id,
         applicants: [],
         imageIds: jobImages.map((image) => image.filename)
     });
-
+    
     //TODO Validate request fields like delievryDate, etc.
     //TODO Consider best way to store images - (practice is to store in a FS like GridFS)
-    job.insertOne(job).then((err) => {
+    job.save((err) => {
         console.error('Error while saving job', err)
-        if (err !== null) throw errors.Error(400, errors.INVALID_JOB_RECEIVED, err);
-    });
-    return job;
-}
-
-async function updateJob(id, jobData, jobImages) {
-    let job = JobModel({
-        ...jobData, // TODO Sanitize input
-        imageIds: jobImages.map((image) => image.filename)
-    });
-
-    //TODO Validate request fields like delievryDate, etc.
-    //TODO Consider best way to store images - (practice is to store in a FS like GridFS)
-    job.updateOne({_id: ObjectId(id)}, job).then((err) => {
-        console.error('Error while updating job', err)
         if (err !== null) throw errors.Error(400, errors.INVALID_JOB_RECEIVED, err);
     })
     return job;
 }
 
-async function deleteJob(id) {
-    let job = JobModel({});
+async function updateJob(user, jobId, jobData, jobImages) {
+    // TODO Authenticate user owns the job
+    // TODO Think about a plan for updating images
+    console.info('updateJob service triggered')
+    console.log('Images:', jobImages);
+    console.log('Data: ', jobData);
 
-    job.deleteOne({_id: ObjectId(id)}).then((err) => {
-        console.error('Error while deleting job', err)
-        if (err !== null) throw errors.Error(400, errors.JOB_NOT_FOUND, err);
-    });
-    return job;
+    let originalJob = JobModel.findByIdAndUpdate(jobId, jobData);
+    return originalJob;
 }
 
+async function getJob(jobId) {
+    // TODO Confirm if any auth is required
+    // TODO Send images
+    
+    return JobModel.findById(jobId);
+}
 
 /**
  * JobService that interacts with the job documents in the database
@@ -53,5 +45,5 @@ async function deleteJob(id) {
 module.exports = {
     createJob,
     updateJob,
-    deleteJob
+    getJob
 }
