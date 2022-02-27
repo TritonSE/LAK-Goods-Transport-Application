a6fd17d94bd2df7ccf684eb3940d798a1ec242ce
Aman Aggarwal <akaggarw@ucsd.edu>
2022-02-27 14:02:41 -0800

    Add error handling, change image storage, and WIP add update and get endpoints

---
+++ backend/errors.js
@@ -0,0 +1,79 @@
+/**
+ * Please add all errors here.
+ * 
+ * For examples, check TSE Octavian - https://github.com/TritonSE/TSE-Oktavian/blob/main/backend/services/errors.js
+ */
+
+// Error messages
+export const INVALID_OBJECT_ID_RECEIVED_MSG = 'Invalid ID was found, ID must be a string of 12 bytes or a string of 24 hex characters';
+export const INVALID_JOB_RECEIVED_MSG = 'Invalid job payload was received, please make sure to have all required job fields';
+export const JOB_NOT_FOUND_MSG = 'Job ID you specified does not exist';
+export const JOB_EDIT_PERMISSION_DENIED_MSG = 'You do not have the right permissions to edit the requested job';
+export const INTERNAL_ERROR_MSG = 'An internal server error occured';
+
+/**
+ * Base error class enherited by all custom errors
+ */
+export class CustomError {
+  constructor(code, status, message) {
+    this.code = code;
+    this.statusCode = status;
+    this.statusMessage = message;
+    this.context = [];
+  }
+
+  /**
+   * Adds context to the error. For example, adding stack trace of the error
+   * @param {*} ctx Context for error
+   * @returns this
+   */
+  addContext(ctx) {
+    this.context.push(ctx);
+    return this;
+  }
+
+  /**
+   * Formats the error depending on where it is being displayed
+   * @param {Boolean} clientFacing Flag which determines where error will be displayed
+   * @returns Error Message
+   */
+  format(clientFacing) {
+    let message = null;
+    if (clientFacing) {
+      message = `ERROR: ${this.statusMessage}`;
+    } else {
+      message = `ERROR: Type ${this.constructor.name}, Code ${this.code}, CONTEXT - ${this.context.length ? '\n' + this.context.join('\n\n') : null }`;
+    }
+    return message;
+  }
+}
+
+/**
+ * Validation Error contains errors that appear while validating client inputs
+ */
+export class ValidationError extends CustomError {
+  static INVALID_OBJECT_ID = new ValidationError(0, 400, INVALID_OBJECT_ID_RECEIVED_MSG);
+}
+
+/**
+ * ServiceError are errors which appear while processing the client's request
+ */
+export class ServiceError extends CustomError {
+    static INVALID_JOB_RECEIVED = new ServiceError(0, 400, INVALID_JOB_RECEIVED_MSG)
+    static JOB_NOT_FOUND = new ServiceError(1, 404, JOB_NOT_FOUND_MSG)
+    static JOB_EDIT_PERMISSION_DENIED = new ServiceError(2, 403, JOB_EDIT_PERMISSION_DENIED_MSG)    
+    static INVALID_JOB_UPDATE = new ServiceError(3, 400, INVALID_JOB_RECEIVED_MSG);
+}
+
+/**
+ * InternalError are those that are not relevant to the client
+ */
+export class InternalError extends CustomError {
+  static IMAGE_UPLOAD_ERROR = new InternalError(0, 500);
+  static IMAGE_DELETE_ERROR = new InternalError(1, 500);
+
+  constructor(code, status, message) {
+    if (!message) message = INTERNAL_ERROR_MSG;
+    super(code, status, message);
+  }
+}
--- backend/routes/job.js
+++ backend/routes/job.js
@@ -1,47 +1,56 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import multer from 'multer'; 
 
-import uploadImage from '../middlewares/upload';
 import {
     createJob,
     updateJob,
     getJob
 } from '../services/job';
-import { INVALID_OBJECT_ID_RECEIVED_MSG } from './errors';
+import { ValidationError } from '../errors';
 import { SAMPLE_USER } from '../constants';
 
 const routes = express.Router();
 
-routes.post('/jobs', uploadImage, (req, res) => { // Create a new job
-    console.info('Posting new Job')
-    // console.log('req: ', req.body); // Request text parameters  
-    // console.log('req:', req.files) // Images in request
-    console.log(SAMPLE_USER);
+// Middleware that parses multipart/form-data request and extracts images into memory storage
+const upload = multer({ storage: multer.memoryStorage() }).array("images");
+
+routes.post('/jobs', upload, (req, res, next) => {
+    console.debug('Posting new Job')
+    // console.log(SAMPLE_USER);
+
     createJob(
         SAMPLE_USER,
         req.body,
         req.files,
-    ).then((job) => {res.status(200).send(job)})
+    )
+    .then((job) => {res.status(200).send(job)})
+    .catch(next);
 });
 
-routes.put('/jobs/:jobid', uploadImage, (req, res) => {
+routes.put('/jobs/:jobid', upload, (req, res, next) => {
+    console.debug('Updating job:', req.params.jobid)
+
     if (!mongoose.Types.ObjectId.isValid(req.params.jobid)) {
-        res.status(400).json({message: INVALID_OBJECT_ID_RECEIVED_MSG})
+        next(ValidationError.INVALID_OBJECT_ID); 
+        return;
     }
     const jobId = mongoose.Types.ObjectId(req.params.jobid);
-    console.info('Updating job with ID: ', jobId);
 
     updateJob(
         SAMPLE_USER,
         jobId,
         req.body,
         req.files,
-    ).then((originalJob) => {res.status(501).send()})
+    ).then(() => {res.status(200).send('Job successfully updated')})
+    .catch(next);
 });
 
 routes.get('/jobs/:jobid', (req, res) => {
+    console.debug('Getting job:', req.params.jobid);
+
     if (!mongoose.Types.ObjectId.isValid(req.params.jobid)) {
-        res.status(400).json({message: INVALID_OBJECT_ID_RECEIVED_MSG})
+        next(ValidationError.INVALID_OBJECT_ID);
     }
     const jobId = mongoose.Types.ObjectId(req.params.jobid);
     console.info('Getting job with ID: ', jobId);
