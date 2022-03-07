df52fb0dbce92328898503cf1c65d9367e46462a
Aman Aggarwal <akaggarw@ucsd.edu>
2022-03-07 04:59:55 -0800

    Update testing args

--- backend/constants.js
+++ backend/constants.js
@@ -1,14 +1,14 @@
 import User from './models/user';
 
 export const SAMPLE_USER_PAYLOAD = {
-  firstName: 'Test',
+  firstName: 'Trucker',
   lastName: 'User',
   password: 'password',
   phone: 'number',
   location: 'location',
 };
 
-export const SAMPLE_USER_ID = '62252de05a86addc14f97018';
+export const SAMPLE_USER_ID = '622600fe04c7a13b6e9b6933';
 
 export const SAMPLE_USER = User({
   firstName: 'Test',
