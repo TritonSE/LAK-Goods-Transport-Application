ddafc620c9f15207d5b5b72a59677d809b2f0728
Aman Aggarwal <akaggarw@ucsd.edu>
2022-03-06 17:49:24 -0800

    closes #27: Update job and user schema

--- backend/models/job.js
+++ backend/models/job.js
@@ -1,22 +1,33 @@
+/**
+ * Schema for Job document
+ */
+
 import mongoose from "mongoose";
+
 const { Schema, model } = mongoose;
 
+
+// Job Status
+export const JOB_STATUS_CREATED = 'CREATED';
+export const JOB_STATUS_ASSIGNED = 'ASSIGNED';
+export const JOB_STATUS_COMPLETED = 'COMPLETED';
+export const JOB_STATUS = [JOB_STATUS_CREATED, JOB_STATUS_ASSIGNED, JOB_STATUS_COMPLETED];
+
+// Stored for validation during update request
+export const FIELDS_OWNER_PERMITTED_TO_UPDATE = ['title', 'clientName', 'phoneNumber', 'deliveryDate', 'pickupLocation', 'dropoffLocation', 'description', 'packageQuantity', 'price'];
+
 const JobSchema = new Schema({
+    // Client monitored attributes
     title: {
         type: String, 
         required: true,
     },
-    clientName: { // TODO Confirm - Client Name should not be same as client logged in?
+    clientName: {
         type: String,
         required: true,
     },
-    phoneNumber: { // TODO Confirm - Do we need a seperate phone or should it the number on record for the client?
+    phoneNumber: {
         type: String,
-        required: true,
-    },
-    client: {
-        type: mongoose.Types.ObjectId,
-        ref: "User",
         required: true,
     },
     deliveryDate: {
@@ -43,27 +54,41 @@ const JobSchema = new Schema({
         type: Number,
         required: true,
     },
-    applicants: [
-        {
+
+    // Server monitored attributes
+    client: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    status: {
+        type: String, 
+        enum: JOB_STATUS,
+        required: true
+    },
+    applicants: [{
+        userId: {
             type: mongoose.Types.ObjectId,
             ref: "User",
-            required: false,
+            required: true,
         },
-        {
-            type: String, // Application date
-            required: false,
+        applyDate: { 
+            type : Date, 
+            required: true,
         }
-    ],
+    }],
     imageIds: [
         {
             type: String,
-            required: false,
+            required: true,
         }
     ],
+    assignedDriver: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+        required: false,
+    },
 });
-
-// Stored for validation during update request
-export const FIELDS_OWNER_PERMITTED_TO_UPDATE = ['title', 'clientName', 'phoneNumber', 'deliveryDate', 'pickupLocation', 'dropoffLocation', 'description', 'packageQuantity', 'price'];
 
 const JobModel = model('Job', JobSchema);
 export default JobModel;
