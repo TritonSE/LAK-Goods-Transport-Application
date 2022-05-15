6ff6bb5c5e6f7447d0ba13de34e6ae2c6f7a6cc1
Kevin Fu <51866248+kgfu02@users.noreply.github.com>
2022-05-14 23:26:27 -0700

    changed screen to match newer version

--- LAKMobile/src/screens/DetailsScreen.tsx
+++ LAKMobile/src/screens/DetailsScreen.tsx
@@ -14,10 +14,11 @@ export function DetailsScreen({jobId}: DetailsScreenProps) {
     // temporary dummy data
     jobId = "62307de6ec658d185f81dff3";
     const [jobData, setJobData] = useState(null);
+    const [driverData, setDriverData] = useState(null);
 
     const getJobData = async () => {
         // replace "10.0.2.2" with localhost if using web
-        const data = await fetch("http://10.0.2.2:3000/api/jobs/" + jobId, {
+        await fetch("http://localhost:3000/api/jobs/" + jobId, {
             method: "GET",
             mode: "cors",
             headers: {
@@ -25,9 +26,21 @@ export function DetailsScreen({jobId}: DetailsScreenProps) {
             },
         }).then(async response => {
             let json = await response.json()
-            console.log(json.job.clientName)
             setJobData(json);
+            await fetch("http://localhost:3000/api/users/get-by-ids", {
+                method: "POST",
+                mode: "cors",
+                headers: {
+                    "content-type": "application/json",
+                },
+                body: JSON.stringify({userIds: [json.job.assignedDriverId]}),
+            }).then(async response => {
+                let json = await response.json();
+                setDriverData(json);
+            })
         })
+
+
     }
 
     React.useEffect(() => {
@@ -42,42 +55,55 @@ export function DetailsScreen({jobId}: DetailsScreenProps) {
                 <View>
                 <View style={styles.detailsComponent}>
                     <AppText style={styles.jobText}>{jobData?.job.title}</AppText>
-                    <AppText style={styles.pickDropComponent}>
-                        Pick up <DetailsArrow></DetailsArrow> Drop Off
-                    </AppText>
-                    <View style={styles.colContainer}>
-                        <AppText>
-                            <Text style={{fontWeight: "bold"}}>Deliver by date{"\n"}</Text>
-                            {jobData?.job.deliveryDate}
-                        </AppText>
-                        {jobData?.job.price ? (
-                            <AppText style={styles.deliverPrice}>
-                                <Text style={{fontWeight: "bold"}}>Delivery Price{"\n"}</Text>
-                                {jobData?.job.price}
-                            </AppText>
-                        ):null}
-                    </View>
 
+                    <View style={styles.fieldContainer}>
+                        <AppText style={{fontWeight: "bold"}}>Pick-up:</AppText>
+                        <AppText>{jobData?.job.pickupLocation}</AppText>
+                    </View>
+                    <View style={styles.fieldContainer}>
+                        <AppText style={{fontWeight: "bold"}}>Drop-off:</AppText>
+                        <AppText>{jobData?.job.dropoffLocation}</AppText>
+                    </View>
+                    <View style={styles.fieldContainer}>
+                        <AppText style={{fontWeight: "bold"}}>Deliver by date:</AppText>
+                        <AppText>{jobData?.job.deliveryDate}</AppText>
+                    </View>
                     {jobData?.job.packageQuantity ? (
-                        <AppText style={styles.packageQuantity}>
-                            Package Quantity: {jobData?.job.packageQuantity}
-                        </AppText>
+                        <View style={styles.fieldContainer}>
+                            <AppText>
+                                <AppText style={{fontWeight: "bold"}}>Package Quantity:</AppText> {jobData?.job.packageQuantity}
+                            </AppText>
+                        </View>
+                    ):null}
+                    {jobData?.job.price ? (
+                        <View style={styles.fieldContainer}>
+                            <AppText>
+                                <AppText style={{fontWeight: "bold"}}>Delivery Price:</AppText> {jobData?.job.price}
+                            </AppText>
+                        </View>
                     ):null}
                     {jobData?.job.description ? (
                         <View style={styles.fieldContainer}>
-                            <AppText style={{fontWeight: "bold"}}>Description</AppText>
+                            <AppText style={{fontWeight: "bold"}}>Description:</AppText>
                             <AppText>{jobData?.job.description}</AppText>
                         </View>
                     ):null}
 
                     <View style={styles.fieldContainer}>
-                        <AppText style={{fontWeight: "bold"}}>Contact</AppText>
+                        <AppText style={{fontWeight: "bold"}}>Contact:</AppText>
                         <AppText>{jobData?.job.clientName}</AppText>
                         <AppText onPress={() => {
                             Linking.openURL('tel:' + jobData?.job.phoneNumber);
                         }}
                                  style={styles.phoneNumber}>
                             {jobData?.job.phoneNumber}
+                        </AppText>
+                        <AppText>{driverData?.users[0].firstName} {driverData?.users[0].lastName}</AppText>
+                        <AppText onPress={() => {
+                            Linking.openURL('tel:' + driverData?.users[0].phone);
+                        }}
+                                 style={styles.phoneNumber}>
+                            {driverData?.users[0].phone}
                         </AppText>
                     </View>
                 </View>
@@ -131,7 +157,6 @@ const styles = StyleSheet.create({
     },
 
     packageQuantity: {
-        marginTop: 20,
         fontWeight: "700",
         lineHeight: 19,
     },
