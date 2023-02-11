597821cdfffb2d1f611837a8440cdf40f549d7de
Arnav710 <63532613+Arnav710@users.noreply.github.com>
2023-02-11 13:04:20 -0800

    changed styling using flexbox for login page

--- AdminPortal/src/pages/login.tsx
+++ AdminPortal/src/pages/login.tsx
@@ -6,141 +6,32 @@ import eyeOpenLogo from '../../public/open-eye.svg';
 import eyeClosedLogo from '../../public/closed-eye.svg';
 import { Martian_Mono } from '@next/font/google';
 
-const styles = {
-  LAAKTAlogo: {
-    width: '445.31px',
-    height: '445.31px',
-    position: 'absolute',
-    left: '147px',
-    top: '206px',
-  },
-
-  formContainer: {
-    position: 'absolute',
-    width: '554.91px',
-    height: '496.64px',
-    left: '756.71px',
-    top: '247.62px',
-
-    background: '#FFFFFF',
-    'box-shadow': '0px 8.32361px 41.6181px rgba(0, 0, 0, 0.25)',
-    'border-radius': '27.7454px',
-  },
-
-  emailPassword: {
-    display: 'flex',
-    'flex-direction': 'column',
-  },
-
-  emailInput: {
-    'box-sizing': 'border-box',
-    background: '#FFFFFF',
-    color: 'black',
-    border: '1.38727px solid #C4C4C4',
-    width: '416.18px',
-    height: '38.84px',
-    margin: '69.36px 69.36px 0px 69.36px',
-    'padding-left': '10px',
-    'border-radius': '5px',
-  },
-
-  passwordInput: {
-    'box-sizing': 'border-box',
-    background: '#FFFFFF',
-    color: 'black',
-    border: '1.38727px solid #C4C4C4',
-    width: '416.18px',
-    height: '38.84px',
-    margin: '15.26px 69.36px 0px 69.36px',
-    'padding-left': '10px',
-    'border-radius': '5px',
-  },
-
-  loginButton: {
-    width: '416px',
-    height: '45px',
-
-    background: '#94100C',
-
-    margin: '17.04px 69.36px 0px 69.36px',
-
-    color: 'white',
-
-    'border-radius': '5px',
-
-    cursor: 'pointer',
-  },
-
-  rememberUserAndForgotPassword: {
-    display: 'flex',
-    margin: '18.03px 69.36px 0px 69.36px',
-  },
-
-  checkbox: {
-    'box-sizing': 'border-box',
-    width: '16.65px',
-    height: '16.65px',
-    border: '1.38727px solid #000000',
-    margin: '0px 7px 0px 0px',
-    cursor: 'pointer',
-  },
-
-  keepSignedInText: {
-    'font-family': 'Lato',
-    'font-style': 'normal',
-    'font-weight': '400',
-    'font-size': '16.6472px',
-    'line-height': '20px',
-    color: 'black',
-    'letter-spacing': '0.2px',
-  },
-
-  forgotPasswordText: {
-    'font-family': 'Lato',
-    'font-style': 'normal',
-    'font-weight': '400',
-    'font-size': '16.6472px',
-    'line-height': '20px',
-    color: 'black',
-    align: 'right',
-    'margin-left': 'auto',
-    'letter-spacing': '0.2px',
-  },
-
-  passwordLogo: {
-    width: '18.03px',
-    height: '18.03px',
-    cursor: 'pointer',
-    'margin-left': '-100px',
-    'padding-top': '4px',
-  },
-};
+import styles from "@/styles/Login.module.css";
 
 export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <div style={styles.main}>
+    <div className={styles.outer}>
       <Image
         src={LAAKTAlogo}
         alt="LAAKTAlogo"
-        className="LAAKTAlogo"
-        style={styles.LAAKTAlogo}
+        className={styles.LAAKTAlogo}
       />
 
-      <div className="formContainer" style={styles.formContainer}>
+      <div className={styles.formContainer}>
         <form>
-          <div style={styles.emailPassword}>
+          <div className={styles.emailPassword}>
             <input
               type="email"
               placeholder="Email"
-              style={styles.emailInput}
+              className={styles.emailInput}
               required
             />
             <p>
               <input
                 placeholder="Password"
-                style={styles.passwordInput}
+                className={styles.passwordInput}
                 type={showPassword ? 'text' : 'password'}
                 required
               />
@@ -149,23 +40,22 @@ export default function Login() {
                 onClick={() => setShowPassword((prevState) => !prevState)}
                 src={showPassword ? eyeOpenLogo : eyeClosedLogo}
                 alt="passwordLogo"
-                className="passwordLogo"
-                style={styles.passwordLogo}
+                className={styles.passwordLogo}
               />
             </p>
           </div>
 
-          <div style={styles.rememberUserAndForgotPassword}>
+          <div className={styles.rememberUserAndForgotPassword}>
             <div>
-              <input style={styles.checkbox} type="checkbox" />
-              <span style={styles.keepSignedInText}>Keep me signed in</span>
+              <input className={styles.checkbox} type="checkbox" />
+              <span className={styles.keepSignedInText}>Keep me signed in</span>
             </div>
-            <a href="#" style={styles.forgotPasswordText}>
+            <a href="#" className={styles.forgotPasswordText}>
               Forgot password
             </a>
           </div>
 
-          <button type="submit" style={styles.loginButton}>
+          <button type="submit" className={styles.loginButton}>
             Login
           </button>
         </form>
