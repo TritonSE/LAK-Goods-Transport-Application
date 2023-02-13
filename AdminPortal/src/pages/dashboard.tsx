4783b5dc5c443181eafd624c0e36a56013915989
Arnav710 <63532613+Arnav710@users.noreply.github.com>
2023-02-12 18:07:27 -0800

    created the driver registeration dashboard

--- AdminPortal/src/pages/dashboard.tsx
+++ AdminPortal/src/pages/dashboard.tsx
@@ -1,6 +1,116 @@
-/**
- * Renders the {url}/dashboard page.
- */
-export default function Dashboard() {
-  return <div>{'This is the dashboard!'}</div>;
+import React, { useState, useEffect } from 'react';
+import styles from "@/styles/Dashboard.module.css";
+import Dropdown from 'react-bootstrap/Dropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import data from '../data/data.json'
+
+interface DataItem {
+  id: number;
+  dateApplied: string;
+  name: string;
+  mobileNumber: string;
+  licenseID: string;
+  licensePlate: string;
+}
+
+export default function App() {
+  const [activeTab, setActiveTab] = useState<string>('Needs_Review');
+  const [isOpen, setIsOpen] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('../data/data.json');
+      const data = await response.json();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  const handleTabClick = (tab: string) => {
+    setActiveTab(tab);
+    fetchData()
+  };
+
+  const toggleDropdown = () => {
+    setIsOpen(prevState => !prevState);
+  };
+
+  return (
+    <div className={styles.outer}>
+      <h1 className={styles.title}>Driver Registeration</h1>
+
+      <div className={styles.tabs}>
+        <button
+          className={activeTab !== 'Needs_Review' ? styles.tabsButton : styles.tabsButtonSelected}
+          onClick={() => handleTabClick('Needs_Review')}
+        >
+          Needs Review
+        </button>
+        <button
+          className={activeTab !== 'In_Review' ? styles.tabsButton : styles.tabsButtonSelected}
+          onClick={() => handleTabClick('In_Review')}
+        >
+          In Review
+        </button>
+        <button
+          className={activeTab !== 'Verified' ? styles.tabsButton : styles.tabsButtonSelected}
+          onClick={() => handleTabClick('Verified')}
+        >
+          Verified
+        </button>
+        <button
+          className={activeTab !== 'Disapproved' ? styles.tabsButton : styles.tabsButtonSelected}
+          onClick={() => handleTabClick('Disapproved')}
+        >
+          Disapproved
+        </button>
+      </div>
+
+      <hr className={styles.horizontalLine}></hr>
+
+      <div className={styles.dropdownAndExportBar}>
+        <Dropdown>
+          <Dropdown.Toggle className={styles.dropdown}>
+            Change Status To
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu className={styles.dropdownItems}>
+            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <button className={styles.exportButton}>
+          Export to CSV File
+        </button>
+
+        </div>
+
+      <table>
+        <thead className={styles.tableHead}>
+          <tr>
+            <th>Date Applied</th>
+            <th>Name</th>
+            <th>Mobile Number</th>
+            <th>License ID</th>
+            <th>License Plate #</th>
+          </tr>
+        </thead>
+        <tbody className={styles.tableBody}>
+          {data.map(item => (
+            <tr key={item.id}>
+              <td className={styles.tableData}>{item.dateApplied}</td>
+              <td className={styles.tableData}>{item.name}</td>
+              <td className={styles.tableData}>{item.mobileNumber}</td>
+              <td className={styles.tableData}>{item.licenseID}</td>
+              <td className={styles.tableData}>{item.licensePlate}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+    </div>
+      
+  )
 }
