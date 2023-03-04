eaea4e12ead3f1d013a4e3e6cc5163003bd893fe
Arnav Modi <arnav@Arnavs-MacBook-Pro-2.local>
2023-03-04 12:17:43 -0800

    ran the linting script

--- AdminPortal/src/pages/login.tsx
+++ AdminPortal/src/pages/login.tsx
@@ -5,61 +5,59 @@ import LAKTAAlogo from '../../public/Logo.svg';
 import eyeOpenLogo from '../../public/open-eye.svg';
 import eyeClosedLogo from '../../public/closed-eye.svg';
 
-import styles from "@/styles/Login.module.css";
+import styles from '@/styles/Login.module.css';
 
 export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
     <div className={styles.outer}>
-      <Image
-        src={LAKTAAlogo}
-        alt="LAKTAAlogo"
-        className={styles.LAKTAAlogo}
-      />
+      <Image src={LAKTAAlogo} alt="LAKTAAlogo" className={styles.LAKTAAlogo} />
 
       <div className={styles.formContainer}>
         <div className={styles.formAllContents}>
-        <form>
-          <div className={styles.emailPassword}>
-            <input
-              type="email"
-              placeholder="Email"
-              className={`${styles.textInputField} ${styles.emailInput}`}
-              required
-            />
-            <p>
+          <form>
+            <div className={styles.emailPassword}>
               <input
-                placeholder="Password"
-                className={`${styles.textInputField} ${styles.passwordInput}`}
-                type={showPassword ? 'text' : 'password'}
+                type="email"
+                placeholder="Email"
+                className={`${styles.textInputField} ${styles.emailInput}`}
                 required
               />
+              <p>
+                <input
+                  placeholder="Password"
+                  className={`${styles.textInputField} ${styles.passwordInput}`}
+                  type={showPassword ? 'text' : 'password'}
+                  required
+                />
 
-              <Image
-                onClick={() => setShowPassword((prevState) => !prevState)}
-                src={showPassword ? eyeOpenLogo : eyeClosedLogo}
-                alt="passwordLogo"
-                className={styles.passwordLogo}
-              />
-            </p>
-          </div>
-
-          <div className={styles.rememberUserAndForgotPassword}>
-            <div>
-              <input className={styles.checkbox} type="checkbox" />
-              <span className={styles.keepSignedInText}>Keep me signed in</span>
+                <Image
+                  onClick={() => setShowPassword((prevState) => !prevState)}
+                  src={showPassword ? eyeOpenLogo : eyeClosedLogo}
+                  alt="passwordLogo"
+                  className={styles.passwordLogo}
+                />
+              </p>
             </div>
-            <a href="#" className={styles.forgotPasswordText}>
-              Forgot password
-            </a>
-          </div>
 
-          <button type="submit" className={styles.loginButton}>
-            Login
-          </button>
-        </form>
-      </div>
+            <div className={styles.rememberUserAndForgotPassword}>
+              <div>
+                <input className={styles.checkbox} type="checkbox" />
+                <span className={styles.keepSignedInText}>
+                  Keep me signed in
+                </span>
+              </div>
+              <a href="#" className={styles.forgotPasswordText}>
+                Forgot password
+              </a>
+            </div>
+
+            <button type="submit" className={styles.loginButton}>
+              Login
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
