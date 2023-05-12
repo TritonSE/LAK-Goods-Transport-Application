b14906f2bb2ac3eff22833d5ea7c7ae4977ab5e8
Vinayak Sharma <03vinsha@gmail.com>
2023-05-12 16:19:11 -0700

    worked on add admin page

--- AdminPortal/src/pages/add-admin.tsx
+++ AdminPortal/src/pages/add-admin.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import styles from '@/styles/add-admin.module.css';
+import { Sidebar } from '../components/sidebar';
+import Link from 'next/link';
 
 function AddAdminPage() {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Add Admin</h1>
-      <p className={styles.description}>This is the page to add an admin.</p>
-    </div>
+    <>
+    <main className={styles.main}>
+        <Sidebar currentPage={'/add-admin'} />
+        <div>
+          <Link href="/admin">
+              <button className={styles.button}>{'Back'}</button>
+          </Link>
+        </div>
+        <div className={styles.header}>{'Add Admin'}</div>
+        <div>
+          <div className={styles.title}>{'Name'}</div>
+          <input className={styles.input} type="text"/>
+        </div>
+        <div>
+          <div className={styles.title}>{'Mobile number'}</div>
+          <input className={styles.input} type="text"/>
+        </div>
+        <Link href="/add-admin">
+            <button className={styles.button2}>{'Add'}</button>
+        </Link>
+      </main>
+    </>
+
+
+    // <div className={styles.container}>
+    //   <h1 className={styles.title}>Add Admin</h1>
+    //   <p className={styles.description}>This is the page to add an admin.</p>
+    // </div>
   );
 }
 
