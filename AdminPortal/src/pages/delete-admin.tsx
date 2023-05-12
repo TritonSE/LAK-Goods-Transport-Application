71a2dc1442eb2f50f73d0304b7e4ef2dedc4e967
Arnav Modi <arnav@Arnavs-MacBook-Pro-2.local>
2023-05-11 18:07:39 -0700

    creating tsx and css files to delete admin

---
+++ AdminPortal/src/pages/delete-admin.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '@/styles/delete-admin.module.css';
+
+function DeleteAdminPage() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Delete Admin</h1>
+      <p className={styles.description}>This is the page to delete an admin.</p>
+    </div>
+  );
+}
+
+export default DeleteAdminPage;
