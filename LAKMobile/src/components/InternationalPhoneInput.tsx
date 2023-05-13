055c418463a19784f69d8e15b0367c5601035612
Andrew Masek <portoaj@users.noreply.github.com>
2023-05-12 17:18:07 -0700

    Cleaned up styling

--- LAKMobile/src/components/InternationalPhoneInput.tsx
+++ LAKMobile/src/components/InternationalPhoneInput.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import PhoneInput from 'react-native-phone-number-input';
 import { COLORS } from '../../constants';
 
@@ -15,52 +14,21 @@ export function InternationalPhoneInput({ setPhoneNumber }: InternationalPhoneIn
         setPhoneNumber(text);
       }}
       containerStyle={{
-        borderWidth: 1,
-        borderRadius: 4,
-        borderColor: COLORS.mediumGrey,
-        borderStyle: 'solid',
         backgroundColor: 'transparent',
       }}
       textContainerStyle={{
         borderWidth: 1,
         borderRadius: 4,
-        borderColor: COLORS.mediumGrey,
         backgroundColor: 'transparent',
+        borderColor: COLORS.gray,
       }}
       flagButtonStyle={{
+        backgroundColor: 'transparent',
+        borderColor: COLORS.gray,
         borderWidth: 1,
         borderRadius: 4,
-        borderColor: COLORS.mediumGrey,
       }}
       autoFocus
     />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#F5C7C5',
-    padding: 15,
-    marginTop: 20,
-    borderRadius: 5,
-    width: '100%',
-    flex: 1,
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 14,
-    textAlign: 'center',
-    lineHeight: 18,
-    fontWeight: '700',
-  },
-  button: {
-    marginTop: 10,
-    width: 'auto',
-    height: 'auto',
-    borderRadius: 5,
-    paddingTop: 10,
-    paddingBottom: 10,
-    paddingLeft: 30,
-    paddingRight: 30,
-  },
-});
