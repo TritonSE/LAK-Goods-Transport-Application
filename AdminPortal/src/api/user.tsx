e26a2e627eac61f79b1dad54ebbc2688293cbe2f
Alex Zhang <alexzhang1618@gmail.com>
2023-09-01 15:35:54 -0700

    Admin Portal - Add driver verification page (#162)
    * Add driver page
    
    * Finish driver verification page

--- AdminPortal/src/api/user.tsx
+++ AdminPortal/src/api/user.tsx
@@ -1,6 +1,24 @@
 import { Option } from '@/pages/dashboard';
 
+export interface UserData {
+  phone: string;
+  firstName: string;
+  lastName: string;
+  location: string;
+  driverLicenseId?: string; // Hidden when anyone else other than the user viewing
+  dateApplied: string;
+  vehicleData?: {
+    vehicleType: string;
+    vehicleModel: string;
+    vehicleMake: string;
+    vehicleColor: string;
+    imageIds: string[];
+  };
+  verificationStatus?: string;
+}
+
 const USERS_URL = `${process.env.NEXT_PUBLIC_API_URL}/api/users`;
+
 export const getAllDrivers = async () => {
   try {
     const url = `${USERS_URL}/get-all-users?`;
@@ -20,7 +38,7 @@ export const getAllDrivers = async () => {
   }
 };
 
-export const updateUser = async (
+export const updateUserStatus = async (
   userId: string,
   verificationStatus: Option
 ) => {
@@ -40,6 +58,44 @@ export const updateUser = async (
     const data = await response.json();
     return { userId: data.userId };
   } catch {
+    return null;
+  }
+};
+
+export const getUser = async (userId: string): Promise<UserData | null> => {
+  try {
+    const url = `${USERS_URL}/${userId}`;
+    const response = await fetch(url);
+    let data = await response.json();
+    data = data.user as UserData;
+    return data;
+  } catch {
+    return null;
+  }
+};
+
+export const getImageURL = (imageId: string): string => {
+  return `${process.env.NEXT_PUBLIC_API_URL}/api/images/${imageId}`;
+};
+
+export const updateUser = async (
+  userId: string,
+  user: UserData
+): Promise<{ userId: string } | null> => {
+  try {
+    console.log(user);
+    const url = `${USERS_URL}/${userId}`;
+    const response = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    });
+    const data = await response.json();
+    return { userId: data.userId };
+  } catch (error) {
+    console.error(error);
     return null;
   }
 };
