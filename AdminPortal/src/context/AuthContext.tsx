11053ac097f57d5ff8268914cf6ad77f3bb0c51b
Alex Zhang <alexzhang1618@gmail.com>
2023-09-05 21:35:45 -0700

    setup for deployment (#164)

--- AdminPortal/src/context/AuthContext.tsx
+++ AdminPortal/src/context/AuthContext.tsx
@@ -6,10 +6,18 @@ import {
   User,
 } from 'firebase/auth';
 
-import firebaseConfig from '../../firebase-config.json';
 import React, { createContext, useEffect, useMemo, useState } from 'react';
 import { FirebaseError } from '@firebase/util';
 import { deleteCookie, getCookie, hasCookie, setCookie } from 'cookies-next';
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
 
 const AUTH_COOKIE = 'lak-admin-user';
 export type AuthState = {
